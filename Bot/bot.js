59a7ef51028e97dc51c818a452fe38f3fb4b55bb
Vitalik <bodnar.vietal@gmail.com>
2024-04-14 02:25:26 +0300

    update bot

--- Bot/bot.js
+++ Bot/bot.js
@@ -5,13 +5,13 @@ const TOKEN = '6553934688:AAEEx2NacO7jqciX6nxevfz3218zux6Ons8'
 const bot = new Telegraf(TOKEN);
 
 
-const web_link = 'https://master--tgfranko.netlify.app/'
+const web_link = 'https://tg-webapp-react-bot.netlify.app'
 
 
 
 bot.start((ctx) =>
   ctx.reply('Welcome :))))))', {
-    reply_markup: { keyboard: [[{ text: "web app", web_app: {url: web_link}}]] },
+    reply_markup: { keyboard: [[{ text: "3D Models", web_app: {url: web_link}}]] },
   })
 );
 
